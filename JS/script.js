2b39189a9e636f9c239d562d7065f97db3e06cf0
Roseverae <cindytrihan@gmail.com>
2025-05-01 00:28:21 +0700

    day 3

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,40 @@
+// konversi suhu
+function konversi() {
+  let celcius =
+  document.getElementById('celcius').value;
+    if (celcius === '') {
+    alert('Isi suhu celcius dulu!');
+    return;
+  }
+  let fahrenheit = (celcius * 9 / 5) + 32;
+
+document.getElementById('fahrenheit').value= fahrenheit.toFixed(2);
+
+document.getElementById('caraKalkulasi').value = `(${celcius} x 9 / 5) + 32 = $
+{fahrenheit.toFixed(2)} °F`;
+}
+
+function reset() {
+  document.getElementById('celcius').value = '';
+  document.getElementById('fahrenheit').value = '';
+  document.getElementById('caraKalkulasi').value = '';
+}
+
+function reverse() {
+  let fahrenheit =
+  document.getElementById('fahrenheit').value;
+  if (fahrenheit === '') {
+    alert('Isi suhu fahrenheit dulu!');
+    return;
+  }
+  let celcius = (fahrenheit - 32) * 5 / 9;
+
+document.getElementById('celcius').value = celcius.toFixed(2);
+
+document.getElementById('caraKalkulasi').value = `(${fahrenheit} - 32) x 5/9 = $
+{celcius.toFixed(2)} °C`;
+}
+
 // Function Auto Slide
 var myIndex = 0;
 carousel();
@@ -13,3 +50,4 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 2000);
 }
+
